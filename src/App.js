2464e48a520464b34f2d9c2c5d2504683052c81a
aleksej_kandyba <aleksejkandyba@gmail.com>
2021-07-28 13:54:10 +0300

    Homework for lesson 2

--- src/App.js
+++ src/App.js
@@ -1,21 +1,53 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react'
+
+// import logo from './logo.svg';
 import './App.css';
 import Message from './components/Message'
-
-
+import Form from './components/Form'
 
 function App() {
 
-  const str = "Изучение React предполагает наличие некоторых знаний о программировании на языке JavaScript. Глубоких знаний не потребуется, но учить React и JavaScript одновременно может быть тяжело.";
+  const [messageList, setMessageList] = useState([]);
+
+  const [value, setValue] = useState('');
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    setValue(e.target.value);
+  };
+
+  const handleMessage = () => {
+    const newMessage = { author: "User", text: value };
+    setMessageList([...messageList, newMessage]);
+    setValue('');
+  };
+
+  useEffect(() => {
+    const id = setInterval(() => {
+      if (messageList.length && messageList[messageList.length - 1].author !== 'Robot') {
+        const robotMess = { author: 'Robot', text: 'hello' };
+        setMessageList([...messageList, robotMess]);
+      }
+    }, 1500);
+    return () => clearInterval(id);
+  }, [messageList]);
+
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Message text={str} />
+        <Message
+          messageList={messageList}
+        />
+        <Form
+          handleMessage={handleMessage}
+          handleChange={handleChange}
+          value={value}
+        />
       </header>
     </div>
   );
 }
 
 export default App;
+
--- src/components/Message.js
+++ src/components/Message.js
@@ -1,8 +1,16 @@
 import './Message.css'
 
 function Message(props) {
+
     return (
-        <p className="messageText">{props.text}</p>
+        <div className="messagesBox">
+            {props.messageList.map((mess) => (
+                <div className="message">
+                    <p className="message__author">{mess.author}:</p>
+                    <p className="message__text">{mess.text}</p>
+                </div>
+            ))}
+        </div>
     )
 };
 
